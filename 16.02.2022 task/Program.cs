915d7fc593a691d8858a364df7255d83862768eb
evezmemmedli <avazkm@code.edu.az>
2022-02-15 20:21:40 +0400

    Add project files.

---
+++ 16.02.2022 task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _16._02._2022_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string mixLetters = "SALAM usaqlar";
+            string newMixLetters = "";
+
+            for (int i = 0; i < mixLetters.Length; i++)
+            {
+                if (char.IsUpper(mixLetters[i]))
+                {
+                    newMixLetters += mixLetters[i].ToString().ToLower();
+
+                }
+                else
+                {
+                    newMixLetters += mixLetters[i].ToString().ToUpper();
+
+                }
+
+            }
+            Console.WriteLine(newMixLetters);
+
+        }
+    }
+}
